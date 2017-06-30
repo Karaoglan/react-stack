e0c295c270dfdac92caf01437f0c708636bcdbd0
Burak Karaoglan <e0825659@student.tuwien.ac.at>
2017-06-30 16:36:25 +0300

    adding bar material design

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import MessageList from './MessageList.jsx';
+import mui from 'material-ui';
+import PropTypes from 'prop-types';
+import {blue500} from 'material-ui/styles/colors';
+import {blue700} from 'material-ui/styles/colors';
+import {blue100} from 'material-ui/styles/colors';
+import {pink400} from 'material-ui/styles/colors';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: blue500,
+    primary2Color: blue700,
+    primary3Color: blue100,
+    accent1Color: pink400
+  },
+  appBar: {
+    height: 50,
+  },
+});
+
+// MuiThemeProvider takes the theme as a property and passed it down the hierarchy.
+const Main = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <AppBar title="My AppBar" />
+  </MuiThemeProvider>
+);
 
 class App extends React.Component {
   constructor(props) {
    super(props);
-   this.state = {
-    messages: [
-      'Hite there, how are you?',
-      'fine and you?'
-    ]
-   };
+   injectTapEventPlugin();
   }
 
   render() {
-    return <MessageList />;
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <AppBar title="Awesome Chat App" />
+      </MuiThemeProvider>
+    );
   }
 }
 
